9dcaf7ecbee7b6106d69afd6407056f5d2163c4f
Devulak <Devulak@users.noreply.github.com>
2018-09-24 23:24:21 +0200

    Added a visualizer per player and tried to fix a problem

--- lua/voicechat.lua
+++ lua/voicechat.lua
@@ -1,8 +1,9 @@
+local theme = shrun.theme;
 
-local PANEL = {}
+local PANELR = {}
 local PlayerVoicePanels = {}
 
-function PANEL:Init()
+function PANELR:Init()
 
 	self.LabelName = vgui.Create( "DLabel", self )
 	self.LabelName:SetFont( "GModNotify" )
@@ -18,12 +19,12 @@ function PANEL:Init()
 
 	self:SetSize( 250, 32 + 8 )
 	self:DockPadding( 4, 4, 4, 4 )
-	self:DockMargin( 2, 2, 2, 2 )
+	self:DockMargin(0, 4, 0, 0)
 	self:Dock( BOTTOM )
 
 end
 
-function PANEL:Setup( ply )
+function PANELR:Setup( ply )
 
 	self.ply = ply
 	self.LabelName:SetText( ply:Nick() )
@@ -35,14 +36,70 @@ function PANEL:Setup( ply )
 
 end
 
-function PANEL:Paint( w, h )
+function PANELR:Paint( w, h )
 
 	if ( !IsValid( self.ply ) ) then return end
-	draw.RoundedBox( 4, 0, 0, w, h, Color( 0, self.ply:VoiceVolume() * 255, 0, 240 ) )
+	local voice = self.ply:VoiceVolume();
+	voice = 1;
+	voice = math.Rand(.1, 1);
+
+	if self.Timer == nil then
+		self.Timer = 0;
+	end
+	self.Timer = self.Timer + RealFrameTime()*16;
+
+	if self.Bars == nil then
+		self.Bars = {};
+	end
+
+	while math.ceil(self.Timer) > #self.Bars do
+		table.insert(self.Bars, voice);
+	end
+
+	//draw.RoundedBox( 4, 0, 0, w, h, Color( 0, voice * 255, 0, 240 ) )
+	draw.RoundedBox( 4, 0, 0, w, h, theme.bgAlternative )
+	//draw.RoundedBox( 4, 0, h - 8, voice * w, 8, Color( 0, voice * 255, 0, 240 ) )
+
+	for k,v in pairs(self.Bars) do
+		if k != 1 then
+			local width = 4
+			local offset = 0
+			local outerMargin = 4
+			local gap = 2
+
+			local xPos = (k - 1 - self.Timer) * (width + gap) + w + offset;
+			local yPos = h - v * (h - outerMargin*2) - outerMargin;
+			local xPos2 = (k - 2 - self.Timer) * (width + gap) + w + offset;
+			local yPos2 = h - self.Bars[k-1] * (h - outerMargin*2) - outerMargin;
+
+			// Bars
+			/*surface.SetDrawColor(v * 255, 255 - v * 255, 0, 255)
+			surface.DrawRect(xPos, yPos, width, v * (h - outerMargin*2))/**/
+
+			// Bars Unicolor
+			surface.SetDrawColor(theme.bg)
+			surface.DrawRect(xPos, yPos, width, v * (h - outerMargin*2))/**/
+
+
+			surface.SetDrawColor(theme.blue)
+
+			// Hills
+			/*surface.DrawLine(xPos2, yPos2, xPos, yPos);/**/
+
+			// Sequence
+			/*if k % 2 == 0 then
+				surface.DrawLine(xPos2, (yPos2 + outerMargin) / 2, xPos, h - (yPos + outerMargin) / 2);
+			else
+				surface.DrawLine(xPos2, h - (yPos2 + outerMargin) / 2, xPos, (yPos + outerMargin) / 2);
+			end/**/
+		end
+	end
+
+	draw.RoundedBox(4, 0, 0, 40, 40, theme:Transparency(theme.blue, voice));
 
 end
 
-function PANEL:Think()
+function PANELR:Think()
 	
 	if ( IsValid( self.ply ) ) then
 		self.LabelName:SetText( self.ply:Nick() )
@@ -54,13 +111,12 @@ function PANEL:Think()
 
 end
 
-function PANEL:FadeOut( anim, delta, data )
+function PANELR:FadeOut( anim, delta, data )
 	
 	if ( anim.Finished ) then
-	
 		if ( IsValid( PlayerVoicePanels[ self.ply ] ) ) then
 			PlayerVoicePanels[ self.ply ]:Remove()
-			PlayerVoicePanels[ self.ply ] = nil
+			PlayerVoicePanels[ self.ply ] = nil;
 			return
 		end
 		
@@ -70,11 +126,11 @@ function PANEL:FadeOut( anim, delta, data )
 
 end
 
-derma.DefineControl( "VoiceNotify", "", PANEL, "DPanel" )
+derma.DefineControl( "VoiceNotifyR", "", PANELR, "DPanel" )
 
 
 
-function PlayerStartVoice( ply )
+function ShrunPlayerStartVoice( ply )
 
 	if ( !IsValid( g_VoicePanelList ) ) then return end
 	
@@ -97,14 +153,16 @@ function PlayerStartVoice( ply )
 
 	if ( !IsValid( ply ) ) then return end
 
-	local pnl = g_VoicePanelList:Add( "VoiceNotify" )
+	local pnl = g_VoicePanelList:Add( "VoiceNotifyR" )
 	pnl:Setup( ply )
 	
 	PlayerVoicePanels[ ply ] = pnl
 
+	return false;
+
 end
 
-local function VoiceClean()
+local function VoiceCleanR()
 
 	for k, v in pairs( PlayerVoicePanels ) do
 	
@@ -115,9 +173,9 @@ local function VoiceClean()
 	end
 
 end
-timer.Create( "VoiceClean", 10, 0, VoiceClean )
+timer.Create( "VoiceCleanR", 10, 0, VoiceCleanR )
 
-function PlayerEndVoice( ply )
+function ShrunPlayerEndVoice( ply )
 
 	if ( IsValid( PlayerVoicePanels[ ply ] ) ) then
 
@@ -128,19 +186,38 @@ function PlayerEndVoice( ply )
 
 	end
 
+	return false;
+
 end
 
-local function CreateVoiceVGUI()
+local function ShrunCreateVoiceVGUI()
 
 	g_VoicePanelList = vgui.Create( "DPanel" )
 
 	g_VoicePanelList:ParentToHUD()
 	g_VoicePanelList:SetPos( ScrW() - 300, 100 )
 	g_VoicePanelList:SetSize( 250, ScrH() - 200 )
-	g_VoicePanelList:SetPaintBackground( true )
+	g_VoicePanelList.Paint = function(self, w, h)
+		if shrun.BottomRightHeight then
+			local width = theme.rem * 15;
+			g_VoicePanelList:SetPos(ScrW() - width -  theme.rem, theme.rem * 18);
+			g_VoicePanelList:SetSize(width, ScrH() - theme.rem * 19 - shrun.BottomRightHeight)
+		end
+		draw.RoundedBox(theme.round, 0, 0, self:GetWide(), self:GetTall(), theme:Transparency(theme.bg, .5));
+	end
+
+	return false;
 
 end
 
-hook.Add( "InitPostEntity", "CreateVoiceVGUI", CreateVoiceVGUI )
-hook.Add( "PlayerEndVoice", "PlayerEndVoiceVGUI", PlayerEndVoice )
-hook.Add( "PlayerStartVoice", "PlayerStartVoiceVGUI", PlayerStartVoice )
+//hook.Remove("InitPostEntity", "CreateVoiceVGUI");
+/*hook.Add( "InitPostEntity", "ShrunCreateVoice", ShrunCreateVoiceVGUI )
+hook.Add( "PlayerEndVoice", "ShrunPlayerEndVoice", ShrunPlayerEndVoice )
+hook.Add( "PlayerStartVoice", "ShrunPlayerStartVoice", ShrunPlayerStartVoice )*/
+
+local HideElements = {"CHudVoiceStatus", "CHudVoiceSelfStatus"}
+hook.Add( "HUDShouldDraw", "ShrunRemoveVoice", function(Element)
+	if table.HasValue(HideElements, Element) then
+		return false;
+	end
+end)
